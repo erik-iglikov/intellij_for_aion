34b8f30727c8eab6e5a530443d4dffa1002d6b29
Alexandra <alexandra@nuco.io>
2018-04-04 15:18:35 -0400

    updating files

---
+++ UpdateScript.sh
@@ -0,0 +1,21 @@
+aion
+
+rsync -rv ../dev_aion/.idea/ ../intellij_for_aion/.idea/
+cp ../dev_aion/aion_fastvm/modFastVM/modFastVM.iml ../intellij_for_aion/aion_fastvm/modFastVM/modFastVM.iml
+cp ../dev_aion/aion_api/modAionApi.iml ../intellij_for_aion/aion_api/modAionApi.iml
+cp ../dev_aion/modAionBase/modAionBase.iml ../intellij_for_aion/modAionBase/modAionBase.iml
+cp ../dev_aion/modAionImpl/modAionImpl.iml ../intellij_for_aion/modAionImpl/modAionImpl.iml
+cp ../dev_aion/modAion/modAion.iml ../intellij_for_aion/modAion/modAion.iml
+cp ../dev_aion/modApiServer/modApiServer.iml ../intellij_for_aion/modApiServer/modApiServer.iml
+cp ../dev_aion/modBoot/modBoot.iml ../intellij_for_aion/modBoot/modBoot.iml
+cp ../dev_aion/modCrypto/modCrypto.iml  modCrypto/modCrypto.iml
+cp ../dev_aion/modDbImpl/modDbImpl.iml ../intellij_for_aion/modDbImpl/modDbImpl.iml
+cp ../dev_aion/modEvtMgrImpl/modEvtMgrImpl.iml ../intellij_for_aion/modEvtMgrImpl/modEvtMgrImpl.iml
+cp ../dev_aion/modEvtMgr/modEvtMgr.iml ../intellij_for_aion/modEvtMgr/modEvtMgr.iml
+cp ../dev_aion/modLogger/modLogger.iml ../intellij_for_aion/modLogger/modLogger.iml
+cp ../dev_aion/modMcf/modMcf.iml ../intellij_for_aion/modMcf/modMcf.iml
+cp ../dev_aion/modP2pImpl/modP2pImpl.iml ../intellij_for_aion/modP2pImpl/modP2pImpl.iml
+cp ../dev_aion/modP2p/modP2p.iml ../intellij_for_aion/modP2p/modP2p.iml
+cp ../dev_aion/modRlp/modRlp.iml ../intellij_for_aion/modRlp/modRlp.iml
+cp ../dev_aion/modTxPoolImpl/modTxPoolImpl.iml ../intellij_for_aion/modTxPoolImpl/modTxPoolImpl.iml
+cp ../dev_aion/modTxPool/modTxPool.iml ../intellij_for_aion/modTxPool/modTxPool.iml
